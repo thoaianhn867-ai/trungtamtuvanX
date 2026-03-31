f17124c420c50461994080e439d8db61244f9110
thoaianhn867-ai <thoaianhn867@gmail.com>
2026-03-31 11:54:22 +0700

    Update chatbot-widget.tsx

--- components/chatbot-widget.tsx
+++ components/chatbot-widget.tsx
@@ -192,7 +192,8 @@ export function ChatbotWidget() {
         {/* Iframe chatbot */}
         <div className="flex-1 relative">
           <iframe
-            src="https://cdn.botpress.cloud/webchat/v3.5/shareable.html?configUrl=https://files.bpcontent.cloud/2026/01/24/14/20260124142242-0E3ZCWBR.json"
+            src="https://cdn.botpress.cloud/webchat/v3.6/shareable.html?conf…s.bpcontent.cloud/2026/03/28/05/20260328051717-LZPMOQCH.json
+"
             className="absolute inset-0 w-full h-full border-0"
             title="Chatbot tư vấn pháp lý"
           />
